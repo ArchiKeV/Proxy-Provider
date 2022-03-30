0b012e2e5c72242d538ac4d087cf028fceeb79c9
ArchiKeV <j0qdc17ts@mozmail.com>
2022-03-30 12:34:18 +0200

    fix: Add Path|expire_on_commit|load|write|next_use
    Apply Path from the pathlib lib for cross-platform import path of the
    server load module
    
    Use "expire_on_commit=False" in session to access object data outside
    session
    
    Add "load_proxy", "write_proxy", "next_use" fields for better visual
    statistics

--- modules/source_loader.py
+++ modules/source_loader.py
@@ -3,6 +3,7 @@ from threading import Thread, Event, Timer
 from datetime import datetime, timedelta
 from operator import itemgetter
 from loguru import logger
+from pathlib import Path
 import importlib.util
 
 from modules.cfg_load import RootConfig
@@ -40,33 +41,36 @@ def source_loader(config: RootConfig, sm_db_sem, db_session: sessionmaker, sm_pr
         thread_text_compositor.start()
 
         while sm_process_status.value:
-            logger.debug('In "source_loader", start "while"')
             for source in sources_list:
                 # 1. Check timer
                 if not source.get("last_use", False):
                     with sm_db_sem:
-                        with db_session.begin() as ses:
-                            source_db: SourceTimer = ses.query(SourceTimer).filter(SourceTimer.name == source.name).first()
+                        with db_session(expire_on_commit=False) as ses:
+                            source_db: SourceTimer = ses.query(SourceTimer).filter(
+                                SourceTimer.name == source['name']
+                            ).first()
                             if not source_db:
                                 ses.add(SourceTimer(name=source['name']))
                                 source_db: SourceTimer = ses.query(SourceTimer).filter(
-                                    SourceTimer.name == source.name
+                                    SourceTimer.name == source['name']
                                 ).first()
                             logger.debug(f"{type(source_db.ts)} {source_db.ts}")
                             if source_db.ts:
                                 source["last_use"] = source_db.ts
-                ts_load = datetime.now() - timedelta(seconds=source.timer)
+                            ses.commit()
+                ts_load = datetime.now() - timedelta(seconds=source['timer'])
                 if not source.get("last_use", False) or ts_load > source.get("last_use", False):
                     # 2. Load source
                     source.update({"status": "NOW"})
                     source_list_update_event.set()
-                    spec = importlib.util.spec_from_file_location("source_module", source['module_path'])
+                    spec = importlib.util.spec_from_file_location("source_module", Path("sources", f"{source['name']}.py"))
                     source_module = importlib.util.module_from_spec(spec)
                     spec.loader.exec_module(source_module)
                     proxy_list: list = source_module.get_proxy(config.proxy.country_code_ignore_list)
-                    source.update({"load_proxy": len(proxy_list)})
+                    load_count = len(proxy_list)
+                    source.update({"load_proxy": load_count})
                     source_list_update_event.set()
-                    logger.debug(f"Source: {source.name} | Load: {len(proxy_list)}")
+                    logger.debug(f"Source: {source['name']} | Load: {load_count}")
                     with sm_db_sem:
                         with db_session.begin() as ses:
                             delete_proxy_list = []
@@ -80,28 +84,42 @@ def source_loader(config: RootConfig, sm_db_sem, db_session: sessionmaker, sm_pr
                             for d_proxy in delete_proxy_list:
                                 proxy_list.remove(d_proxy)
                             if len(proxy_list) > 0:
-                                proxy_write = ses.add_all([
+                                ses.add_all([
                                     Proxy(**proxy) for proxy in proxy_list
                                 ])
-                                if proxy_write > 0:
-                                    sm_new_proxy_event.set()
-                    source.update({"write_proxy": proxy_write})
-                    logger.debug(f"Source: {source.name} | Load: {len(proxy_list)} | Write: {proxy_write}")
+                                sm_new_proxy_event.set()
+                    source.update({"write_proxy": len(proxy_list)})
+                    logger.debug(f"Source: {source['name']} | Load: {load_count} | Write: {len(proxy_list)}")
                     source_list_update_event.set()
                     # 3. Update timer in DB
                     with sm_db_sem:
                         last_use = datetime.now()
-                        with db_session.begin():
+                        with db_session() as ses:
+                            source_db = ses.merge(source_db)
                             source_db.ts = last_use
+                            ses.commit()
                     # 4. Update timer in dict
-                    source.update({"last_use": last_use})
-                    logger.debug(f"Source: {source.name} | Load: {len(proxy_list)} | Write: {proxy_write} | {last_use}")
+                    next_use = last_use + timedelta(seconds=source['timer'])
+                    source.update({
+                        "last_use": last_use,
+                        "next_use": next_use,
+                        "status": "LOADED"})
+                    logger.debug(
+                        f"Source: {source['name']} | Load: {load_count} | Write: {len(proxy_list)} | {last_use.isoformat(' ', 'seconds')}"
+                    )
                     source_list_update_event.set()
-                # 5. Sorted sources_list from 'last_use' in source
-                time_to_next_load = ((source['last_use'] + timedelta(seconds=source['timer'])) - datetime.now()).seconds
-                logger.debug(f"Source {source['name']}will be checked in {time_to_next_load} seconds")
+                    time_to_next_load = (next_use - datetime.now()).seconds
+                    logger.debug(f"Source {source['name']} will be checked in {time_to_next_load} seconds")
+                else:
+                    if not source.get('next_use'):
+                        next_use = source['last_use'] + timedelta(seconds=source['timer'])
+                        source.update({
+                            "next_use": next_use})
+                        source_list_update_event.set()
+            # 5. Sorted sources_list from 'last_use' in source
             sources_list = sorted(sources_list, key=itemgetter('last_use'))
-            second_timer = (datetime.now() - sources_list[0]['last_use']).seconds
+            second_timer = (datetime.now() - sources_list[0]['next_use']).seconds
+            logger.debug(f'Timer {second_timer}')
             thread_unlock_event.clear()
             t = Timer(second_timer, thread_event_unlock, args=(thread_unlock_event, ))
             t.start()
@@ -122,41 +140,55 @@ def text_compositor(
         sm_process_status, sm_tui_buffer, config, sm_tui_change_flag, sm_tui_refresh, sources_list,
         source_list_update_event
 ):
-    table_headers = ['Source name', 'Last use', 'Next use', 'Last n proxy']
+    table_headers = ['Source name', 'Last use', 'Next use', 'Load proxy', 'Write proxy']
     name_length = len(table_headers[0])
     last_use_length = len(table_headers[1])
     next_use_length = len(table_headers[2])
-    last_n_proxy_length = len(table_headers[3])
+    load_proxy_length = len(table_headers[3])
+    write_proxy_length = len(table_headers[4])
     while sm_process_status.value:
         sm_tui_buffer[:] = []
         table_headers_string = f'{table_headers[0]:<{name_length}} ' \
                                f'{table_headers[1]:<{last_use_length}} ' \
                                f'{table_headers[2]:<{next_use_length}} ' \
-                               f'{table_headers[3]:<{last_n_proxy_length}}'
+                               f'{table_headers[3]:<{load_proxy_length}} ' \
+                               f'{table_headers[4]:<{write_proxy_length}}'
         sm_tui_buffer.append(table_headers_string)
         for source in sources_list:
-            prev_lens = (name_length, last_use_length, next_use_length, last_n_proxy_length)
-            if len(source['name']) > name_length:
-                name_length = len(source['name'])
-            if len(str(source['last_use_ts'])) > last_use_length:
-                last_use_length = len(str(source['last_use_ts']))
-            if not source.get("next_use", None):
-                source.update({'next_use': None})
-            if len(str(source['next_use'])) > next_use_length:
-                next_use_length = len(str(source['next_use']))
-            if not source.get("last_n_proxy", None):
-                source.update({'last_n_proxy': None})
-            if len(str(source['last_n_proxy'])) > last_n_proxy_length:
-                last_n_proxy_length = len(str(source['last_n_proxy']))
-            if prev_lens != (name_length, last_use_length, next_use_length, last_n_proxy_length):
+            prev_lens = (name_length, last_use_length, next_use_length, load_proxy_length, write_proxy_length)
+
+            name_string, len_name_string = source['name'], len(source['name'])
+            if source.get('last_use', None): last_use_string = source['last_use'].isoformat(' ', 'seconds')
+            else: last_use_string = 'None'
+            if source.get('next_use', None): next_use_string = source['next_use'].isoformat(' ', 'seconds')
+            else: next_use_string = 'None'
+            if source.get('load_proxy', None): load_string = str(source['load_proxy'])
+            else: load_string = 'None'
+            if source.get('write_proxy', None): write_string = str(source['write_proxy'])
+            else: write_string = 'None'
+
+            len_last_use_string = len(last_use_string)
+            len_next_use_string = len(next_use_string)
+            len_load_string = len(load_string)
+            len_write_string = len(write_string)
+
+            if name_length < len_name_string: name_length = len_name_string
+            if last_use_length < len_last_use_string: last_use_length = len_last_use_string
+            if next_use_length < len_next_use_string: next_use_length = len_next_use_string
+            if load_proxy_length < len_load_string: load_proxy_length = len_load_string
+            if write_proxy_length < len_write_string: write_proxy_length = len_write_string
+
+            if prev_lens != (name_length, last_use_length, next_use_length, load_proxy_length):
                 sm_tui_buffer[0] = f'{table_headers[0]:<{name_length}} ' \
                                    f'{table_headers[1]:<{last_use_length}} ' \
                                    f'{table_headers[2]:<{next_use_length}} ' \
-                                   f'{table_headers[3]:<{last_n_proxy_length}}'
-            source_string = f'{source["name"]:<{name_length}} ' \
-                            f'{str(source["last_use_ts"]):<{last_use_length}} ' \
-                            f'{str(source["next_use"]):<{next_use_length}} ' \
-                            f'{str(source["last_n_proxy"]):<{last_n_proxy_length}}'
+                                   f'{table_headers[3]:<{load_proxy_length}} ' \
+                                   f'{table_headers[4]:<{write_proxy_length}}'
+            source_string = f'{name_string:<{name_length}} ' \
+                            f'{last_use_string:<{last_use_length}} ' \
+                            f'{next_use_string:<{next_use_length}} ' \
+                            f'{load_string:<{load_proxy_length}} ' \
+                            f'{write_string:<{write_proxy_length}}'
             sm_tui_buffer.append(source_string)
         if len(sm_tui_buffer) > config.system.tui_text_line_buffer_size:
             while len(sm_tui_buffer) > config.system.tui_text_line_buffer_size:
